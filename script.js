d50ff50257e47df5eb532ec1ea379a42446daf2f
GlebAdizev <147997603+GlebAdizev@users.noreply.github.com>
2026-09-21 11:51:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function linkHover(element) {
+    element.style.marginLeft = "8px";
+    element.style.fontWeight = "bold";
+}
+
+function linkOut(element) {
+    element.style.marginLeft = "0px";
+    element.style.fontWeight = "normal";
+}
