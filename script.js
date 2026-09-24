d0dce43152d19c90774c713ff961a143419e7640
GlebAdizev <147997603+GlebAdizev@users.noreply.github.com>
2026-09-24 21:43:48 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function linkHover(element) {
-    element.style.marginLeft = "8px";
-    element.style.fontWeight = "bold";
-}
-
-function linkOut(element) {
-    element.style.marginLeft = "0px";
-    element.style.fontWeight = "normal";
-}
+function linkHover(element) { element.style.marginLeft = "8px"; element.style.fontWeight = "bold"; }
+function linkOut(element) { element.style.marginLeft = "0px"; element.style.fontWeight = "normal"; }
+function logoHover(element) { element.style.transform = "translateY(-8px)"; }
+function logoOut(element) { element.style.transform = "translateY(0px)"; }
