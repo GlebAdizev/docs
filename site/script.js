fe1e3912993e098d122ef55f21b8dc1246b9ee2c
GlebAdizev <147997603+GlebAdizev@users.noreply.github.com>
2026-09-24 21:14:52 +0500

    Delete site directory

--- site/script.js
+++
@@ -1,9 +0,0 @@
-function linkHover(element) {
-    element.style.marginLeft = "8px";
-    element.style.fontWeight = "bold";
-}
-
-function linkOut(element) {
-    element.style.marginLeft = "0px";
-    element.style.fontWeight = "normal";
-}
